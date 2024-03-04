25d90b1fd0a10b4a4f346a500bc4d9c8e71c078c
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2024-03-04 14:31:12 +0100

    Daten erstellt

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Eintrag(models.Model):
+    content = models.CharField(("Eintrag"), max_length=80)
+    done = models.BooleanField(("Erledigt"), default = False)
+    created = models.DateTimeField(("Erstellt"), auto_now=False, auto_now_add=True)
+    changed = models.DateTimeField(("Verändert"), auto_now=True, auto_now_add=False)
+    user = models.ForeignKey(User, verbose_name=("Benutzer"), on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = ("Eintrag")
+        verbose_name_plural = ("Einträge")
+
+    def __str__(self):
+        erledigt = ""
+        if self.done:
+            erledigt = "erledigt"
+        else:
+            erledigt = "offen"
+        return f"{self.user} ({str(self.created)}) - {self.content} ({erledigt})"
+
+    #def get_absolute_url(self):
+    #    return reverse("entry_detail", kwargs={"pk": self.pk})
