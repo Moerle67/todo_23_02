cf73f178f6bc762e20ded344f30cc96082469027
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2024-03-04 15:50:20 +0100

    Liste

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def home(request):
+    liste = Eintrag.objects.filter(done=False)
+    content = {
+        'liste': liste
+    }
+    return render(request, "todoapp/todo_list.html", content)
